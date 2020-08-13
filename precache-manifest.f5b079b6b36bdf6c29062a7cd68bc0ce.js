ba0510c6069da00d92dc8f36d29c3705ec5fcea5
Mehul Kothari <34826782+mehulk05@users.noreply.github.com>
2020-08-13 21:08:24 +0530

    Updates

---
+++ precache-manifest.f5b079b6b36bdf6c29062a7cd68bc0ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60797f9e37a57c9f4430766d5a97d8e",
+    "url": "/NewsApp-using-React/index.html"
+  },
+  {
+    "revision": "8d77c6d5e2e8b775cac1",
+    "url": "/NewsApp-using-React/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "486e5d33bb8603cafd18",
+    "url": "/NewsApp-using-React/static/css/main.cd40c144.chunk.css"
+  },
+  {
+    "revision": "8d77c6d5e2e8b775cac1",
+    "url": "/NewsApp-using-React/static/js/2.eac5f449.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewsApp-using-React/static/js/2.eac5f449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486e5d33bb8603cafd18",
+    "url": "/NewsApp-using-React/static/js/main.3307740a.chunk.js"
+  },
+  {
+    "revision": "903062bbd3cf05569883",
+    "url": "/NewsApp-using-React/static/js/runtime-main.ba763ae7.js"
+  }
+]);
